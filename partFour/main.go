0b7fd41037141f9035cf60f61848678934912ea7
Zack Morel <morelz133@gmail.com>
2019-11-18 14:18:35 -0500

    4 concurrent workers

---
+++ partFour/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// buffer channels given a size of 100
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results) // 4 concurrent and uses 4 different cpus. We get to take advantage of the multicore processor
+
+	for i := 0; i < 100; i++ {
+		jobs <- i
+	}
+	close(jobs) // close jobs channel because we have filled up the channel with the input
+
+	for j := 0; j < 100; j++ {
+		// After jobs
+		fmt.Println(<-results)
+	}
+
+}
+
+//           jobs to do    | resuts from channels
+func worker(jobs <-chan int, results chan<- int) {
+	for n := range jobs {
+		results <- fib(n) // feeds number from job in to results channel
+	}
+
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return fib(n-1) + fib(n-2)
+}
